c78ad309b3a264b4b20ea6601aa9b63d2417294d
pumpyboom <31203291+pumpyboom@users.noreply.github.com>
2024-01-25 13:54:21 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/DeliveryStarted.java
@@ -0,0 +1,28 @@
+package labshoppubsub.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshoppubsub.domain.*;
+import labshoppubsub.infra.AbstractEvent;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private Long orderid;
+    private Long productid;
+    private Integer qty;
+    private String status;
+
+    public DeliveryStarted(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryStarted() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
